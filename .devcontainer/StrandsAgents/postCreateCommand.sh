0bd6b24d81cfa94ca8d8ded241df785dcd086aad
Kento.Yamada <ymd65536@outlook.com>
2026-03-27 18:10:47 +0000

    feat: Update postCreateCommand.sh to install strands-agents-tools and add terminal prompt customization

--- .devcontainer/StrandsAgents/postCreateCommand.sh
+++ .devcontainer/StrandsAgents/postCreateCommand.sh
@@ -1,4 +1,7 @@
 # bin/bash
-pip install --no-cache --user --break-system-packages strands-agents 'strands-agents[a2a]'
+pip install --no-cache --user --break-system-packages strands-agents 'strands-agents[a2a]' strands-agents-tools
 pip config set install.user 'false'
 pip config set global.index-url https://pypi.org/simple
+
+# Shorten terminal prompt
+echo 'export PS1="\$ "' >> ~/.bashrc
