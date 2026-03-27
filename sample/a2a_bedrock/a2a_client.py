75566a7afc256325be61d47b5cd960880256b5d6
Kento.Yamada <ymd65536@outlook.com>
2026-03-27 18:32:20 +0000

    fix: a2a client

--- sample/a2a_bedrock/a2a_client.py
+++ sample/a2a_bedrock/a2a_client.py
@@ -5,9 +5,14 @@ a2a_agent = A2AAgent(endpoint="http://localhost:9000")
 
 try:
     # Invoke it just like a regular Agent
-    result = a2a_agent("Show me 10 ^ 6")
-    print(result.message)
+    result = a2a_agent("How is the weather today?", stream=True)
+    # print(result.message)
     # {'role': 'assistant', 'content': [{'text': '10^6 = 1,000,000'}]}
+    if result.message.get("content"):
+        contents = result.message["content"]
+        for content in contents:
+            if content.get("text"):
+                print(content["text"])
 except Exception as e:
-    print(f"Error: {e}")
+    print(f"Error: {e.message}")
 
